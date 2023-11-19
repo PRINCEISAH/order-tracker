15aa8fdb2c05c1858039efcd5a6c12bccb700995
audu isah <37813957+PRINCEISAH@users.noreply.github.com>
2023-11-19 20:45:00 +0100

    remove api keys

--- lib/models/config.dart
+++ lib/models/config.dart
@@ -1,3 +1,1 @@
-const String APIKey =
-    'nwju7g.5R7jpw:BzN3VbLZ4HG_lMFG5m2WT5JCxe-HSBW6E2ClolwdqNM';
-const String OAuthConsumerKey = 'Place your twitter oauth_consumer_key here';
+const String APIKey = 'Place your Ably API key here';
